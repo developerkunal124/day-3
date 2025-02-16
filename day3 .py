374c49717df3338b28a72b4a8c626747b6aff77c
developerkunal124 <kunalraghav153@gmail.com>
2025-02-16 21:40:49 +0530

    first commit

---
+++ day3 .py
@@ -0,0 +1,15 @@
+#string in python
+name="kunal"
+print("hello  "  +name)
+
+
+## string slicing
+name="doctor"
+print(name[0:4])
+
+
+
+#calculate length of any string .
+fruit="apple"
+len1=len(fruit)
+print("apple is a",len1,"letter word")
